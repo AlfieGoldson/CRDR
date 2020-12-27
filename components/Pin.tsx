9343fb2844fac7b9567fc4a945debc5fecf29add
Alfie <djobbo.maiga@gmail.com>
2020-12-28 00:14:53 +0100

    pin animation

--- components/Pin.tsx
+++ components/Pin.tsx
@@ -15,14 +15,11 @@ export default function Pin({ title, sound, reversed }: Props) {
 			style={{ top: 'xd' }}
 			className={`pin-container ${
 				currentSound == title ? 'playing' : ''
-			}`}
+			} ${reversed ? 'reversed' : ''}`}
 			onClick={() => playSound(title, sound)}
 		>
 			<h3>{title}</h3>
-			<svg
-				viewBox='0 0 35.593 32.083'
-				className={`pin ${reversed ? 'reversed' : ''}`}
-			>
+			<svg viewBox='0 0 35.593 32.083' className='pin'>
 				<path d='M35.593,16.041h0a12.018,12.018,0,0,1-7.757,11.237L17.234,31.3A12.018,12.018,0,0,1,1.284,22.844L.327,18.825a12.013,12.013,0,0,1,0-5.567l.956-4.019A12.018,12.018,0,0,1,17.234.785L27.836,4.8a12.018,12.018,0,0,1,7.757,11.237' />
 			</svg>
 		</div>
